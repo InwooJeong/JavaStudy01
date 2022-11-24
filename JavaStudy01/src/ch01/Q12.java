c1447e492de19bfd90104668c1a861c4f878c29f
sinth92 <sinth92@hotmail.com>
2022-11-25 08:32:13 +0900

    need to study about printf, %d, nested loop

---
+++ JavaStudy01/src/ch01/Q12.java
@@ -0,0 +1,14 @@
+package ch01;
+
+//곱셈표를 출력하는 프로그램을 작성하세요.
+public class Q12 {
+
+	public static void main(String[] args) {
+		System.out.println("	|");
+		for(int i=1;i<=9;i++) {
+			System.out.printf("%3d",i);
+		}
+		
+	}
+
+}
