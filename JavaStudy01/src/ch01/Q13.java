60c28014420d07875d46460d651931e55a81f4ca
sinth92 <sinth92@hotmail.com>
2022-11-28 08:42:40 +0900

    Q12, 13

--- JavaStudy01/src/ch01/Q13.java
+++ JavaStudy01/src/ch01/Q13.java
@@ -1,7 +1,7 @@
 package ch01;
 
-//곱셈표를 출력하는 프로그램을 작성하세요.
-public class Q12 {
+//덧셈을 출력하는 프로그램을 작성하세요.
+public class Q13 {
 
 	public static void main(String[] args) {
 		System.out.println("   |");
@@ -13,7 +13,7 @@ public class Q12 {
 		for(int i=1;i<=9;i++) {
 			System.out.printf("%2d |",i);
 			for(int j=1;j<=9;j++) {
-				System.out.printf("%3d",i*j);;
+				System.out.printf("%3d",i+j);;
 			}
 			System.out.println();
 		}
